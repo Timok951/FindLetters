ee89b5be66ef5c4c10389a6553b723d7ac0d6057
Timok951 <timofey.alekseev@list.ru>
2024-06-01 02:10:21 +0300

    Добавьте файлы проекта.

---
+++ FindLetters/FindLetters.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "../DynamicLib/Header.h"
+#include "Windows.h"
+
+
+int main()
+{
+
+	HINSTANCE load;
+
+	load = LoadLibrary(L"DynamicLib.dll");
+	typedef int (*sum) (int, int);
+	sum Sum;
+	Sum = (sum)GetProcAddress(load, "Sum");
+	std::cout << Sum(2, 5);
+	FreeLibrary(load);
+
+}
+
